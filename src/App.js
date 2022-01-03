1f7404acc587eca8ce26591a973a0c80b8162151
Christofer <christoferguima@gmail.comnam>
2022-01-03 16:56:14 -0300

    search method is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import JSON_DATA from "./data/MOCK_DATA.json";
+import "./App.css";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <div>
+        {JSON_DATA.filter((data) => {
+          return searchValue !== ""
+            ? data.first_name.toLowerCase().includes(searchValue.toLowerCase())
+            : data;
+        }).map((data, key) => (
+          <div key={key} className="data">
+            {data.id} - {data.first_name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
